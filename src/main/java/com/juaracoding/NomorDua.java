5a883c42ddb4bbd53190e4431e30c8ca45fa7b78
Putri Tarigan <putrivalenzia14@gmail.com>
2023-08-26 13:03:37 +0700

    Update nomor dua

---
+++ src/main/java/com/juaracoding/NomorDua.java
@@ -0,0 +1,36 @@
+package com.juaracoding;
+
+public class NomorDua {
+    public static void main(String[] args) {
+        // Inisialisasi objek StringBuilder untuk menyimpan pola
+        StringBuilder print = new StringBuilder();
+
+        // Loop pertama untuk mengisi StringBuilder
+        for (int i = 1; i <= 9; i ++) {
+            if (i % 2 == 0) {
+                // Jika i genap, tambahkan underscore ke StringBuilder
+                print.append("_");
+            } else {
+                // Jika i ganjil, tambahkan nilai i ke StringBuilder
+                print.append(i);
+            }
+        }
+
+        // Cetak pola hasil dari loop pertama
+        System.out.println(print);
+
+        // Loop kedua untuk menghapus karakter pertama dan terakhir secara berulang
+        for (int i = 0; i < 9 / 2; i++) {
+            // Panggil metode removeFirstAndLast() untuk mengubah strPrint
+            print = removeFirstAndLast(print);
+            // Cetak hasil dari perubahan strPrint
+            System.out.println(print);
+        }
+    }
+
+    // Metode untuk menghapus karakter pertama dan terakhir dari StringBuilder
+    public static StringBuilder removeFirstAndLast(StringBuilder strBuilder) {
+        // Menggunakan substring untuk mengambil potongan string
+        return new StringBuilder(strBuilder.substring(1, strBuilder.length() - 1));
+    }
+}
